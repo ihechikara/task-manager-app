189af5dfc650529ec5d9b895e63d40162d36eec4
Ihechikara <ihechivinabba@gmail.com>
2023-04-22 16:17:48 +0100

    feat: set up routes and controllers
    Set up route and controller folder along with task.js files for each
    folder

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks")
 
+//port
 const port = 3000;
 
+// middleware
+app.use(express.json())
+
+// routes
 app.get("/", (req, res)=>{
     res.send("Welcome!")
 })
+
+app.use("/api/v1/tasks", tasks)
+
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
---
+++ controllers/tasks.js
@@ -0,0 +1,8 @@
+const getAllTasks = (req, res)=>{
+    res.send("All tasks")
+}
+
+
+module.exports = {
+    getAllTasks
+}
